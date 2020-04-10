12020b4e5e64d6d07a2a55458832d2eb5a47c6f4
Hatsuyuki <hatsuyuki15@gmail.com>
2020-04-10 17:17:29 +0700

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module org.hatsuyuki.tplgen
+
+go 1.13
+
+require (
+	github.com/bmatcuk/doublestar v1.2.2
+	gopkg.in/yaml.v2 v2.2.8
+)
---
+++ tplgen.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"bytes"
+	"github.com/bmatcuk/doublestar"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+func main() {
+	workingDir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	workingDir = filepath.Join(workingDir, "..", "iac")
+	templateDir := filepath.Join(workingDir, "templates")
+
+	globPattern := workingDir + "/**/*.tpl"
+	files, err := doublestar.Glob(globPattern)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, file := range files {
+		log.Println("Processing :" + file)
+		data, err := ioutil.ReadFile(file)
+		if err != nil {
+			log.Fatal("File reading error: "+file, err)
+		}
+		spec := parseSpec(string(data))
+		templatePath := filepath.Join(templateDir, spec.Template)
+		result := evaluateTemplate(spec, templatePath)
+		writeToFile(file, result)
+	}
+}
+
+func writeToFile(file string, content string) {
+	newFile := file + ".yaml"
+	err := ioutil.WriteFile(newFile, []byte(content), 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func evaluateTemplate(spec Spec, templatePath string) string {
+	tmpfile := createTempFile(spec.toString())
+	defer os.Remove(tmpfile.Name())
+
+	cmd := exec.Command("ytt", "-f", templatePath, "-f", tmpfile.Name())
+
+	var out, errOut bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &errOut
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(errOut.String())
+	}
+
+	return out.String()
+}
+
+func createTempFile(content string) *os.File {
+	tmpfile, err := ioutil.TempFile("", "tpl*.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if _, err := tmpfile.Write([]byte(content)); err != nil {
+		tmpfile.Close()
+		log.Fatal(err)
+	}
+
+	if err := tmpfile.Close(); err != nil {
+		log.Fatal(err)
+	}
+	return tmpfile
+}
+
+func parseSpec(data string) Spec {
+	var spec Spec
+	err := yaml.UnmarshalStrict([]byte(data), &spec)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return spec
+}
+
+type Spec struct {
+	Template string
+	Values   yaml.MapSlice
+}
+
+func (s Spec) toString() string {
+	out, err := yaml.Marshal(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+	res := "#@data/values\n#@overlay/match-child-defaults missing_ok=True\n---\n" + string(out)
+	return res
+}
